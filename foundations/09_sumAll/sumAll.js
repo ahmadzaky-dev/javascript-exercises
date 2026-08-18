b1ebb6664297c906708404a16ce6d8e7430b94ae
Zaky <ahmadzaky797@gmail.com>
2026-08-18 19:46:52 +0700

    Complete 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || a <= 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(b) || b <= 0) {
+        return "ERROR";
+    }
 
+    let terkecil = Math.min(a, b);
+    let terbesar = Math.max(a, b);
+    let result = 0;
+    for (let i = terkecil; i <= terbesar; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
